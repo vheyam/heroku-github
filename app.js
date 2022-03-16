f6aaced1e42e4a43aabcd1bcd6000103268cbb22
ismail <vheyam@hotmail.com>
2022-03-16 10:38:25 +0000

    hello world

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const app = express;
+const app = express();
 const PORT = process.env.PORT || 1377;
 
 
 app.use(express.json());
 app.use(express.static('public'));
+console.log('what is going on');
 
 app.listen(PORT, () =>{
     console.log('App running on port ' + PORT);
